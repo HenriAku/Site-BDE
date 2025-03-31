5ba79a7e78a171f6b82a75f9b7f6a5c0800b2374
Wychowski Theo <wychowski.theo@gmail.com>
2025-03-31 14:36:02 +0200

    modif article et ArticleRepo

--- site/app/entities/Article.php
+++ site/app/entities/Article.php
@@ -1,21 +1,30 @@
 <?php
 class Article {
 
-    public function __construct(private ?int $id,
-    private string $name,
-    private float $price,
-    private string $description,
-    private int $stock,
-    private ?Category $category = null) {
+    public function __construct(
+        private ?int $n_produit,
+        private string $name,
+        private int $stock,
+        private string $category,
+        private float $price,
+        private string $description,
+        private string $color,
+        private string $size){}
 
-    }
-
-    public function getId(): ?int {
-        return $this->id;
+    public function getn_produit(): ?int {
+        return $this->n_produit;
     }
 
     public function getName(): string {
         return $this->name;
+    }
+
+    public function getStock(): int {
+        return $this->stock;
+    }
+
+    public function getCategory(): string {
+        return $this->category;
     }
 
     public function getPrice(): float {
@@ -26,12 +35,25 @@ class Article {
         return $this->description;
     }
 
-    public function getStock(): int {
-        return $this->stock;
+    public function getColor(): string {
+        return $this->color;
     }
+
+    public function getSize(): string {
+        return $this->size;
+    }
+
 
     public function setName(string $name): void {
         $this->name = $name;
+    }
+
+    public function setStock(int $stock): void {
+        $this->stock = $stock;
+    }
+
+    public function setCategory(string $category): void {
+        $this->category = $category;
     }
 
     public function setPrice(float $price): void {
@@ -42,18 +64,12 @@ class Article {
         $this->description = $description;
     }
 
-    public function setStock(int $stock): void {
-        $this->stock = $stock;
+    public function setColor(string $color): void {
+        $this->color = $color;
     }
 
-    public function getCategory(): ?Category
-    {
-        return $this->category;
-    }
-
-    public function setCategory(?Category $category): void
-    {
-        $this->category = $category;
+    public function setSize(string $size): void {
+        $this->size = $size;
     }
 }
 ?>
