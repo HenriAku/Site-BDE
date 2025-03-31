0bc92d8b7452868330668cc7541516114bda0aea
Henri Rougeolle <rougeollehenri@gmail.com>
2025-03-31 08:28:10 +0200

    ajout log

--- site/config/config.php
+++ site/config/config.php
@@ -1,7 +1,7 @@
 <?php
 // Définition de constantes pour la configuration de la base de données
 define('DB_HOST', 'woody');  // Hôte de la base de données (généralement localhost)
-define('DB_NAME', 'login');  // Nom de la base de données
-define('DB_USER', 'login');  // Nom d'utilisateur de la base de données
-define('DB_PASS', 'password');  // Mot de passe de la base de données
+define('DB_NAME', 'rh230139');  // Nom de la base de données
+define('DB_USER', 'rh230139');  // Nom d'utilisateur de la base de données
+define('DB_PASS', 'henri02');  // Mot de passe de la base de données
 define('DB_PORT', '5432');  // Mot de passe de la base de données
