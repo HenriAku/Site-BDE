ed74a19c4f41ce7938f194ee5669d00b65fbe87d
yanis <yanisyachir03@gmail.com>
2025-04-02 17:36:30 +0200

    ajout admin dans User

--- site/app/entities/User.php
+++ site/app/entities/User.php
@@ -1,8 +1,16 @@
 <?php
 class User {
    
-    public function __construct(private ?int $id, private string $firstname,private string $lastname,private string $email, private ?string $password, private ?string $netu, private bool $estvalide = false)
-    {}
+    public function __construct(
+        private ?int $id, 
+        private string $firstname,
+        private string $lastname,
+        private string $email, 
+        private ?string $password, 
+        private ?string $netu, 
+        private bool $estvalide = false, 
+        private bool $estadmin = false
+    ) {}
 
     public function getId(): ?int
     {
@@ -69,6 +77,16 @@ class User {
         $this->estvalide = $estvalide;
     }
 
+    public function getAdmin(): bool
+    {
+        return $this->estadmin;
+    }
+
+    public function setAdmin(bool $estadmin): void
+    {
+        $this->estadmin = $estadmin;
+    }
+
     public function __serialize(): array
     {
         return [
@@ -78,20 +96,21 @@ class User {
             'email' => $this->email,
             'password' => $this->password,
             'netu' => $this->netu,
-            'estvalide' => $this->estvalide
+            'estvalide' => $this->estvalide,
+            'estadmin' => $this->estadmin
         ];
     }
 
-    public function __unserialize(array $data):void
+    public function __unserialize(array $data): void
     {
-        $this->id = $data['id'];
-        $this->firstname = $data['firstname'];
-        $this->lastname = $data['lastname'];
-        $this->email = $data['email'];
-        $this->password = $data['password'];
-        $this->netu = $data['netu'];
-        $this->estvalide = $data['estvalide'];
-
+        $this->id = $data['id'] ?? null;
+        $this->firstname = $data['firstname'] ?? '';
+        $this->lastname = $data['lastname'] ?? '';
+        $this->email = $data['email'] ?? '';
+        $this->password = $data['password'] ?? null;
+        $this->netu = $data['netu'] ?? null;
+        $this->estvalide = $data['estvalide'] ?? false;
+        $this->estadmin = $data['estadmin'] ?? false;
     }
 }
 ?>
