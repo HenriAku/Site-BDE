8721c3f4c54865d7fb6c25cbd567755a39868ac4
Fanch Even <fancheventravail@gmail.com>
2025-04-03 16:25:46 +0200

    test mails

--- site/app/controllers/PanierController.php
+++ site/app/controllers/PanierController.php
@@ -1,13 +1,15 @@
 <?php
 require_once './app/core/Controller.php';
 require_once './app/repositories/PanierRepository.php';
+require_once './app/repositories/UserRepository.php';
 require_once './app/trait/FormTrait.php';
 require_once './app/trait/AuthTrait.php';
 require_once './app/services/AuthService.php';
+require_once './vendor/autoload.php'; 
 
 class PanierController extends Controller {
 
-use FormTrait;
+    use FormTrait;
 
     public function index() 
     {
@@ -28,30 +30,47 @@ use FormTrait;
 
     }
 
-    public function achete($userId, $produit_id, $panierQte, $panierId)
-    {
+    public function achete($userId, $produit_id, $panierQte, $panierId) {
         $repo = new PanierRepository();
-
+        $userRepo = new UserRepository();
+        
+        $produit = $repo->getProduitById($produit_id);
+        $user = $userRepo->findById($userId);
+        
         $repo->achete($userId, $produit_id, $panierQte);
-
         $repo->delete($panierId);
+        
+        $this->envoyerMailConfirmation($user, $produit, $panierQte);
     }
 
-    public function acheteToutPanier($userId):bool
-    {
+
+    public function acheteToutPanier($userId): bool {
         $repo = new PanierRepository();
-
+        $userRepo = new UserRepository();
         $paniers = $repo->findAll();
-
-        foreach($paniers as $panier)
-        {
-            if($panier->getn_user() == $userId)
-            {
+        $user = $userRepo->findById($userId);
+        
+        $total = 0;
+        $detailsAchats = [];
+        
+        foreach($paniers as $panier) {
+            if($panier->getn_user() == $userId) {
+                $produit = $repo->getProduitById($panier->getn_produit());
                 $repo->achete($userId, $panier->getn_produit(), $panier->getqte());
                 $repo->delete($panier->getn_panier());
+                
+                $total += $produit->getPrice() * $panier->getqte();
+                $detailsAchats[] = [
+                    'nom' => $produit->getName(),
+                    'quantite' => $panier->getqte(),
+                    'prix_unitaire' => $produit->getPrice(),
+                    'total' => $produit->getPrice() * $panier->getqte()
+                ];
             }
         }
-
+        
+        $this->envoyerMailConfirmationGlobal($user, $detailsAchats, $total);
+        
         return true;
     }
 
