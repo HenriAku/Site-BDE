d93193b7daeea82b3b559c0f9e17cb7a70e4788e
Henri <rougeollehenri@gmail.com>
2025-04-03 12:48:53 +0200

    panel gestion admin

---
+++ site/gestion.php
@@ -0,0 +1,11 @@
+<?php
+require_once './app/core/Controller.php';
+require_once './app/controllers/UserController.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$controller = new UserController();
+
+$controller->admin(); 
