67de49bf5b89b4d0d95b118b6552c6cd1e585016
Fanch Even <fancheventravail@gmail.com>
2025-04-03 18:13:00 +0200

    des insert

---
+++ site/bd/inserts_pertinants.sql
@@ -0,0 +1,25 @@
+INSERT INTO Commente (n_event, n_etu, note, avis) VALUES
+-- Soirée de rentrée (event 11)
+(11, 1, 4, 'Super ambiance mais un peu trop bondé à mon goût'),
+(11, 3, 5, 'Meilleure soirée depuis mon arrivée à la fac ! DJ incroyable'),
+(11, 4, 3, 'Bonne organisation mais choix de musique moyen'),
+
+-- Tournoi jeux vidéo (event 10)
+(10, 2, 5, 'Parfaitement organisé avec du bon matériel'),
+(10, 5, 4, 'Génial mais durée un peu trop courte'),
+
+-- Atelier CV (event 9)
+(9, 1, 5, 'Intervenant très professionnel, j ai appris énormément'),
+(9, 2, 5, 'Atelier qui change la vie ! Mon CV a déjà retenu l attention des recruteurs'),
+(9, 4, 4, 'Très utile mais aurait mérité plus d exercices pratiques'),
+
+-- Week-end ski (event 8)
+(8, 3, 5, 'Séjour parfait, moniteur de ski top et hébergement confortable'),
+(8, 5, 2, 'Problème d organisation pour les remontées mécaniques'),
+
+-- Gala de fin d année (event 12)
+(12, 1, 5, 'Tout simplement magique, digne d un gala prestigieux'),
+(12, 2, 4, 'Très belle soirée mais repas un peu léger'),
+(12, 3, 5, 'Feu d artifique à couper le souffle !'),
+(12, 4, 3, 'Cadre magnifique mais animation un peu faible en milieu de soirée');
+
